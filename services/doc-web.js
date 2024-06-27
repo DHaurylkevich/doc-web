5e305d10cc66d6595820222d8be78ffde249fc81
DimaHaurylkevich <dhaurylkevich@gmail.com>
2024-06-27 16:38:46 +0200

    fix

--- services/doc-web.js
+++ services/doc-web.js
@@ -1,8 +1,13 @@
 const express = require("express");
+
 const app = express();
+
+const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
     res.send("Hello");
 })
 
-app.listen(300)
+app.listen(port, ( ) => console.log(
+    'Express запущен на http://localhost:${port); ' +
+    'нажмите Ctrl+C для завершения.'))
