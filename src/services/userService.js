22ce4593bbc03556aaffa20f376334c7642a086b
DimaHaurylkevich <dhaurylkevich@gmail.com>
2024-09-26 11:55:03 +0200

    Saving changes before switching to a new branch

--- src/services/userService.js
+++ src/services/userService.js
@@ -2,22 +2,28 @@ const { models } = require("../models");
 const User = models.users;
 const medical_centers = models.medical_centers;
 
-
-
-exports.createUser = async (user) => {
-    try {
-        if (user.role === "doctor") {
-            const center = await medical_centers.findOne({ where: { center_id: user.center_id } });
-            if (!center) {
-                throw new Error("Medical center not found");
+const UserService = {
+    createUser: async (userData) => {
+        try {
+            const userFound = await User.findOne({ where: { email: userData.email } });
+            if (userFound) {
+                throw new Error("User already exist");
             }
-        }
 
-        return await User.create(user)
-    } catch (e) {
-        console.error("Error occurred", e);
-        throw new Error(e.message);
+            if (userData.role === "doctor") {
+                const center = await medical_centers.findOne({ where: { center_id: userData.center_id } });
+                if (!center) {
+                    throw new Error("Medical center not found");
+                }
+            }
+
+            return await User.create(userData)
+        } catch (e) {
+            console.error("Error occurred", e);
+            throw new Error(e.message);
+        }
     }
 }
 
-// CreateUser создает пользователя в базе даных. Данные в сервис уже приходят правильными. Можно создать три вида пользователя: Admin, Patient, Doctor. Чтобы чоздать доктора, нужно чтобы был корректный существующий center_id
+module.exports = UserService;
+// CreateUser создает пользователя в базе даных. Данные в сервис уже приходят правильными. Можно создать три вида пользователя: Admin, Patient, Doctor. При создании пользователя проверяется есть ли пользователь уже в Базе Данных. Чтобы cоздать доктора, нужно чтобы был корректный существующий center_id
--- tests/unit/usersService.test.js
+++ tests/unit/usersService.test.js
@@ -2,11 +2,14 @@ require("dotenv").config();
 process.env.NODE_ENV = 'test';
 
 const sinon = require("sinon");
-const { expect } = require("chai");
+const { expect, use } = require("chai");
+const chaiAsPromised = require("chai-as-promised");
 // const sequelize = require("../../src/config/db");
 const { faker } = require('@faker-js/faker');
 const { users, medical_centers } = require("../../src/models").models;
-const userService = require("../../src/services/userService");
+const UserService = require("../../src/services/userService");
+
+use(chaiAsPromised);
 
 describe("Users Service", () => {
     describe("Positive test", () => {
@@ -15,7 +18,7 @@ describe("Users Service", () => {
                 createUserStub = sinon.stub(users, "create");
             })
 
-            beforeEach(async () => {
+            afterEach(async () => {
                 sinon.restore();
             });
 
@@ -29,11 +32,14 @@ describe("Users Service", () => {
                     role: "patient",
                     center_id: null,
                 };
+                createUserStub.resolves(newUser);
 
-                const user = await userService.createUser(newUser);
+                const user = await UserService.createUser(newUser);
+
+                expect(createUserStub.calledWith(newUser)).to.be.true;
                 expect(user).to.be.a("object").that.deep.include(newUser);
             });
-            
+
             it("when user has valid data and role 'doctor', expect user to be create", async () => {
                 const newUser = {
                     first_name: faker.person.firstName(),
@@ -52,7 +58,7 @@ describe("Users Service", () => {
                 findCenterStub.resolves(fakeCenter);
                 createUserStub.resolves(newUser);
 
-                const user = await userService.createUser(newUser);
+                const user = await UserService.createUser(newUser);
 
                 expect(findCenterStub.calledWith({ where: { center_id: 1 } })).to.be.true;
                 expect(createUserStub.calledWith(newUser)).to.be.true;
@@ -63,7 +69,15 @@ describe("Users Service", () => {
     });
     describe("Error test", () => {
         describe("Create user", () => {
-            it("when user is doctor and has invalid center_id", async () => {
+            beforeEach(async () => {
+                createUserStub = sinon.stub(users, "create");
+            })
+
+            afterEach(async () => {
+                sinon.restore();
+            });
+
+            it("when email already is in DB, expect Error with User already exist", async () => {
                 const newUser = {
                     first_name: faker.person.firstName(),
                     last_name: faker.person.lastName(),
@@ -71,12 +85,34 @@ describe("Users Service", () => {
                     password: faker.internet.password(),
                     phone: faker.phone.number(),
                     role: "doctor",
-                    center_id: "",
+                    center_id: 2,
                 }
 
+                findUserStub = sinon.stub(users, "findOne").resolves({ email: newUser.email });
 
+                await expect(UserService.createUser(newUser)).to.be.rejectedWith(Error, "User already exist");
 
+                expect(findUserStub.calledWith({ where: { email: newUser.email } })).to.be.true;
+                expect(createUserStub.notCalled).to.be.true;
+            });
 
+            it("when user is doctor and has invalid center_id, expect Error with Medical center not found", async () => {
+                const newUser = {
+                    first_name: faker.person.firstName(),
+                    last_name: faker.person.lastName(),
+                    email: faker.internet.email(),
+                    password: faker.internet.password(),
+                    phone: faker.phone.number(),
+                    role: "doctor",
+                    center_id: 2,
+                }
+                findUserStub = sinon.stub(users, "findOne").resolves(false);
+                findCenterStub = sinon.stub(medical_centers, "findOne").resolves(null);
+
+                await expect(UserService.createUser(newUser)).to.be.rejectedWith(Error, "Medical center not found");
+
+                expect(findCenterStub.calledWith({ where: { center_id: 2 } })).to.be.true;
+                expect(createUserStub.notCalled).to.be.true;
             })
         });
     });
