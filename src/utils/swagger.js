08c10a1e7a64c12363444ee6b2b6a70c96946197
DimaHaurylkevich <dhaurylkevich@gmail.com>
2024-11-09 01:50:32 +0300

    SWAGGER FIX2

--- src/utils/swagger.js
+++ src/utils/swagger.js
@@ -1,6 +1,8 @@
 const swaggerJsDoc = require('swagger-jsdoc');
 const swaggerUi = require('swagger-ui-express');
+const path = require('path');
 
+// Настройки Swagger
 const swaggerOptions = {
     definition: {
         openapi: "3.0.0",
@@ -8,17 +10,31 @@ const swaggerOptions = {
             title: "MyLekarz",
             version: "1.0.0",
             description: 'Описание API для проекта',
-        }
+        },
     },
-    apis: ['./src/routes/*.js']
+    apis: [path.resolve(__dirname, '../routes/*.js')], // Убедитесь, что путь корректен
 };
 
-const swaggerDocs = swaggerJsDoc(swaggerOptions)
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
 
-const options = {
-    customCss: 'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css'
-}
+const swaggerSetupOptions = {
+    customCssUrl: 'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css',
+};
 
 module.exports = (app) => {
-    app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs, options));
-}
+    app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs, swaggerSetupOptions));
+
+    // Явное указание маршрутов для статики Swagger UI
+    app.get('/swagger-ui-bundle.js', (req, res) => {
+        res.sendFile(require.resolve('swagger-ui-dist/swagger-ui-bundle.js'));
+    });
+
+    app.get('/swagger-ui-standalone-preset.js', (req, res) => {
+        res.sendFile(require.resolve('swagger-ui-dist/swagger-ui-standalone-preset.js'));
+    });
+
+    // Эндпоинт для JSON спецификации API (для Swagger UI)
+    app.get('/swagger.json', (req, res) => {
+        res.json(swaggerDocs);
+    });
+};
