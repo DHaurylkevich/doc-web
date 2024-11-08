0922cab767b0f217be06d567f77d307c4da9c86a
DimaHaurylkevich <dhaurylkevich@gmail.com>
2024-11-08 17:57:49 +0300

    Fix env

--- src/config/db.js
+++ src/config/db.js
@@ -1,7 +1,14 @@
 const { Sequelize } = require("sequelize");
 require("dotenv").config();
 const env = process.env.NODE_ENV || "development";
-const config = require("./sequelize.config.json")[env];
+// const config = require("./sequelize.config.json")[env];
+const config = {
+    username: process.env.DB_USER || "root",
+    password: process.env.DB_PASS || null,
+    database: process.env.DB_NAME || "mylekarz",
+    host: process.env.DB_HOST || "localhost",
+    dialect: "mysql"
+};
 
 let sequelize
 if (config.use_env_variable) {
