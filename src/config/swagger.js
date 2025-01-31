e6f491bc95c56886d9bed13fd353232426a06129
DimaHaurylkevich <dhaurylkevich@gmail.com>
2025-01-31 20:42:23 +0100

    fix swagger 10

--- src/config/swagger.js
+++ src/config/swagger.js
@@ -1,48 +1,16 @@
-const swaggerJsDoc = require('swagger-jsdoc');
+// const swaggerJsDoc = require('swagger-jsdoc');
 const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
 const path = require('path');
 
-const swaggerOptions = {
-    definition: {
-        // openapi: '3.0.0',
-        // info: {
-        //     title: 'MyLekarz API',
-        //     version: '1.0.0',
-        //     description: 'API Documentation for MyLekarz',
-        // },
-        // servers: [
-        //     {
-        //         url: process.env.NODE_ENV !== "production"
-        //             ? "http://localhost:3000/api"
-        //             : "https://doc-web-rose.vercel.app/api",
-        //         description: "Development server"
-        //     },
-        //     {
-        //         url: "https://doc-web-rose.vercel.app/api",
-        //         description: "Production server"
-        //     }
-        // ],
-        // components: {
-        //     securitySchemes: {
-        //         CookieAuth: {
-        //             type: "apiKey",
-        //             in: "cookie",
-        //             name: "token"
-        //         }
-        //     }
-        // },
-    },
-    apis: [path.resolve(__dirname, '../utils/docs/*.yaml')],
-};
-
-const swaggerDocs = swaggerJsDoc(swaggerOptions);
+const swaggerDocument = YAML.load(path.resolve(__dirname, '../utils/docs/swagger.yaml'));
 
 const swaggerSetupOptions = {
     customCssUrl: 'https://unpkg.com/swagger-ui-dist@5/swagger-ui.css',
 };
 
 module.exports = (app) => {
-    app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs, swaggerSetupOptions));
+    app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument, swaggerSetupOptions));
 
     // Явное указание маршрутов для статики Swagger UI
     app.get('/swagger-ui-bundle.js', (req, res) => {
